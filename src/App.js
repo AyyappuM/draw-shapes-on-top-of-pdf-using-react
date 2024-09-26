308aa8c4df1363f19f43ff8036e48cfdf68bcd38
user <user@ubuntu-vm.myguest.virtualbox.org>
2024-09-26 10:33:37 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import React from 'react';
 import PDFViewer from './PDFViewer';
 import samplePdf from './sample.pdf';
 import './App.css';
-import { pdfjs, Document, Page } from 'react-pdf';
 
 const App = () => {
-  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-  ).toString();
-  
   return (
     <div>
       <h1>Simple PDF Viewer with line drawing</h1>
